90a57ec5488ce14e73347e24bad3bc62de730dc7
Chris Baird <chrisbaird05@icloud.com>
2018-09-05 08:54:39 -0700

    More work on front end

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -11,12 +11,24 @@ let viewChange = (event) => {
     switch ($dataAttr) {
         case 'play':
             console.log('1')
+            $('.start-area').addClass('hidden');
+            $('.options-area').removeClass('hidden');
             break;
-        case 'Instructions':
+        case 'instructions-go-back':
             console.log('2')
+            $('.instructions-area').addClass('hidden');
+            $('.main-nav').removeClass('hidden');
+            break;
+        case 'idk':
+            console.log('3')
             break;
         case 'about':
-            console.log('3')
+            console.log('4')
+            break;
+        case 'instructions':
+            console.log('5')
+            $('.nav-links').addClass('hidden');
+            $('.instructions-area').removeClass('hidden');
             break;
         default:
             return;
